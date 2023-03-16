aee1a2c991b339b93efbb8a3240df4417ee52cff
YisusJoe <yisusjoe.mx@gmail.com>
2023-03-15 20:46:46 -0700

    Se arreglo que al presionar primero una tecla de 'operación aritmética' en lugar de aparecer solo el símbolo en la pantalla aparezca un '0' antes de este

--- calculadora.js
+++ calculadora.js
@@ -1,49 +1,72 @@
-let total = 0;
-let valPantalla = '';
+let total = 0
+let valPantalla = ''
 
 function refreshDisplay(totalPantalla) {
 
-    document.getElementById("pantalla").innerHTML = totalPantalla;
+    document.getElementById("pantalla").innerHTML = totalPantalla
 
 }
 
 function clearDisplay() {
 
-    valPantalla = '0';
-    total = 0;
-    document.getElementById("pantalla").innerHTML = valPantalla;
-    valPantalla = '';
+    valPantalla = '0'
+    total = 0
+    document.getElementById("pantalla").innerHTML = valPantalla
+    valPantalla = ''
 
 }
 
 function teclado(boton) {
 
-    let tecla = boton.value;
+    let tecla = boton.value
 
     switch(tecla){
 
         case '+':
-            valPantalla += tecla;
-            refreshDisplay(valPantalla);
+            
+            valPantalla += tecla
+
+            if(valPantalla[0] == '+') {
+                //console.log("El primer caracter es +")
+                valPantalla = '0' + tecla
+            }
+
+            refreshDisplay(valPantalla)
             break;
         case '-':
-            valPantalla += tecla;
-            refreshDisplay(valPantalla);
-            break;
+            valPantalla += tecla
+
+            if(valPantalla[0] == '-') {
+                //console.log("El primer caracter es +")
+                valPantalla = '0' + tecla
+            }
+
+            refreshDisplay(valPantalla)
+            break
         case '*':
-            valPantalla += boton.value;
-            refreshDisplay(valPantalla);
-            break;
+            valPantalla += tecla
+
+            if(valPantalla[0] == '*') {
+                //console.log("El primer caracter es +")
+                valPantalla = '0' + tecla
+            }
+
+            refreshDisplay(valPantalla)
+            break
         case '=':
-            total = eval(valPantalla);
-            valPantalla = total;
-            refreshDisplay(valPantalla);
+            total = eval(valPantalla)
+            valPantalla = total
+            refreshDisplay(valPantalla)
             valPantalla = ''
             //console.log(total);
-            break;
+            break
         default:
             //total += boton.value;
-            valPantalla += tecla;
+            valPantalla += tecla
+            if(valPantalla[0] == '.') {
+                //console.log("El primer caracter es +")
+                valPantalla = '0' + tecla
+            }
             refreshDisplay(valPantalla);
             //console.log(tecla);
     }
